5ed50e081d38d8ce51d80bb7767a4e876b72e050
Asher <ashish.2017@hotmail.com>
2023-11-11 21:34:27 +0000

    Add database with sqlx

---
+++ migrations/20231111154451_user_table.up.sql
@@ -0,0 +1,10 @@
+-- Add up migration script here
+
+CREATE TABLE patient (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    -- email VARCHAR(255) UNIQUE NOT NULL,
+    -- password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,55 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, time::Duration};
 
 use axum::{
+    extract::State,
     http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
     Json, Router,
 };
-
 use serde::{Deserialize, Serialize};
-use tracing;
+use sqlx::postgres::{PgPool, PgPoolOptions};
+use tracing::{self, debug};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
     // initialize tracing
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "example_tokio_postgres=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    //load .env
+    dotenvy::dotenv().unwrap();
+
+    let db_connection_str = std::env::var("DATABASE_URL")
+        .unwrap_or_else(|_| "postgres://postgres:password@localhost".to_string());
+
+    // set up connection pool
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .acquire_timeout(Duration::from_secs(3))
+        .connect(&db_connection_str)
+        .await
+        .expect("can't connect to database");
 
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
         .route("/", get(root))
         // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+        .route("/users", post(create_user))
+        .with_state(pool);
 
     let address = SocketAddr::from(([127, 0, 0, 1], 3000));
-
-    tracing::debug!("listening on {}", address);
+    debug!(
+        "listening on {}, DATABASE_URL={}",
+        address, db_connection_str
+    );
 
     axum::Server::bind(&address)
         .serve(app.into_make_service())
@@ -38,6 +63,7 @@ async fn root() -> &'static str {
 }
 
 async fn create_user(
+    State(pool): State<PgPool>,
     // this argument tells axum to parse the request body
     // as JSON into a `CreateUser` type
     Json(payload): Json<CreateUser>,
@@ -45,8 +71,16 @@ async fn create_user(
     // insert your application logic here
     let user = User {
         id: 1337,
-        username: payload.username,
+        username: payload.username.clone(),
     };
+
+    sqlx::query!(
+        "INSERT INTO patient (username) VALUES ($1) ON CONFLICT (username) DO NOTHING",
+        payload.username
+    )
+    .execute(&pool)
+    .await
+    .unwrap();
 
     // this will be converted into a JSON response
     // with a status code of `201 Created`
